ecc22aafbf61b298d65d0f37ecd242410378bb94
Mark Cockburn <Mork@TheMork.co.uk>
2017-07-29 19:57:56 +0100

    Update module 'decorators'
     - prevent command execution if bot lacks required permissions
     - framework for disabling commands by channel/role

--- decorators.py
+++ decorators.py
@@ -2,6 +2,7 @@ import re
 import logging
 import asyncio
 import discord
+from util import has_permission, truncate
 
 from functools import wraps
 
@@ -9,12 +10,14 @@ log = logging.getLogger('pedantbot')
 
 
 def command(pattern: str = "", db_name: str = None, description: str = "", usage: str = "",
-            cooldown: int = 0, global_cooldown: int = 0):
+            cooldown: int = 0, global_cooldown: int = 0,
+            require_role: str = None, banned_role: str = None,
+            requires_permissions: discord.Permissions = None):
     def actual_decorator(func):
         name = func.__name__
         cmd_name = "/" + name
         prog = re.compile(pattern or cmd_name)
-        nsfw = hasattr(func, 'nsfw') and func.nsfw != False
+        is_nsfw = hasattr(func, 'nsfw') and func.nsfw is not False
 
         @wraps(func)
         async def wrapper(self, message: discord.Message):
@@ -28,16 +31,16 @@ def command(pattern: str = "", db_name: str = None, description: str = "", usage
 
             storage = await self.get_storage(server)
 
-            channel_disabled = await storage.get("cmd_disabled:{}".format(message.channel.id))
+            channel_disabled = await storage.get("channel_disabled:{}".format(message.channel.id))
             if channel_disabled == "1":
                 return
 
             if wrapper.nsfw and "nsfw" not in channel.name or \
-                    (hasattr(channel, 'nsfw') and channel.nsfw != True):
+                    (hasattr(channel, 'nsfw') and channel.nsfw is not True):
                 return
 
             if cooldown:
-                check = await storage.get("cooldown:{}:{}".format(db_name or name,message.author))
+                check = await storage.get("cooldown:{}:{}".format(db_name or name, message.author))
                 if check:
                     return
                 await storage.set("cooldown:{}:{}".format(db_name or name, message.author), 1, expire=cooldown)
@@ -48,21 +51,40 @@ def command(pattern: str = "", db_name: str = None, description: str = "", usage
                     return
                 await storage.set("global_cooldown:{}".format(db_name or name), 1, expire=global_cooldown)
 
+            if requires_permissions:
+                if not has_permission(server.me, requires_permissions):
+                    await self.client.send_message(
+                        channel,
+                        "I do not have permission for that.\nRequires `{}`.".format(requires_permissions)
+                    )
+                    return
+
+            if require_role:
+                role = await storage.get('role:{}'.format(require_role))
+                if role not in [x.id for x in message.author.roles]:
+                    return
+
+            if banned_role:
+                if banned_role in [x.id for x in message.author.roles]:
+                    return
+
             log.info("{}#{}@{} >> {}".format(
                 message.author.name,
                 message.author.discriminator,
                 message.server.name,
-                message.clean_content[:100]
+                truncate(message.clean_content, 100)
             ))
 
             await func(self, message, args)
-            added = await self.client.db.redis.incr('pedant3.stats:command_uses:{}'.format(wrapper.info.get('name', 'unknown')))
+            added = await self.client.db.redis.incr('pedant3.stats:command_uses:{}'.format(
+                wrapper.info.get('name', 'unknown')
+            ))
             if added:
-                await self.client.db.redis.sadd('pedant3.stats:commands',wrapper.info.get('name', 'unknown'))
+                await self.client.db.redis.sadd('pedant3.stats:commands', wrapper.info.get('name', 'unknown'))
 
         wrapper._db_name = db_name or func.__name__
         wrapper._is_command = True
-        wrapper.nsfw = nsfw
+        wrapper.nsfw = is_nsfw
 
         if usage:
             command_name = usage
