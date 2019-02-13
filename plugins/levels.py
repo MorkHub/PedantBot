c3032151f1dcf0a210331ffa716142a4172eda39
MorkHub <mork@themork.co.uk>
2019-02-13 22:49:09 +0000

    Add doublexp mode to levels plugin
    Add doublexp mode to levels plugin

--- plugins/levels.py
+++ plugins/levels.py
@@ -1,5 +1,6 @@
 import logging
 from random import randint
+import datetime
 
 from classes.plugin import Plugin
 from decorators import command
@@ -17,16 +18,16 @@ class Levels(Plugin):
 
     @staticmethod
     def xp_from_level(level):
-        points = 0
-        for i in range(1, level + 1):
-            diff = int(i * 5 ** (1.2*math.e))
-            points += diff
-        return points
+        xp = 0
+        for _level in range(1, level):
+            xp += math.floor(_level + 300 * pow(2, _level / 7))
+        
+        return math.floor(xp / 4)
 
     @staticmethod
     def level_from_xp(xp):
-        level = 0
-        while Levels.xp_from_level(level) < xp:
+        level = 1
+        while Levels.xp_from_level(level + 1) < xp:
             level += 1
         return level
 
@@ -42,17 +43,20 @@ class Levels(Plugin):
         if await storage.get('player:{}:limited'.format(user.id)):
             return
 
-        xp = await storage.get('player:{}:xp'.format(user.id)) or 0
-        before = self.level_from_xp(int(xp))
-        xp = await storage.incrby('player:{}:xp'.format(user.id), randint(15, 25)) or 0
+        double_xp = bool(await storage.get('double_xp:global') or await storage.get('double_xp:{}'.format(channel.id)) or 0)
+
+        added_xp = (len(message.content) / (len(message.content) + 5)) * 30 * (2 if double_xp else 1)
+        xp = await storage.incrby('player:{}:xp'.format(user.id), int(added_xp)) or 0
+
+        before = self.level_from_xp(int(xp - added_xp))
         after = self.level_from_xp(int(xp))
 
         await storage.sadd('players', user.id)
         await storage.set('player:{}:limited'.format(user.id), '1', expire=20)
 
         if after > before:
-            announce = bool(await storage.get('announce_enabled') or '1')
-            if not announce:
+            announce = await storage.get('announce_enabled')
+            if announce == '0':
                 return
 
             await self.client.send_message(
@@ -62,6 +66,68 @@ class Levels(Plugin):
                     level=after
                 )
             )
+
+    @command(pattern="^!doublexp ([0-9]+)(?: (server))?$",
+             description="enable bonus xp in this channel for n days",
+             usage="!doublexp 2")
+    async def doublexp(self, message, args):
+        server = message.server
+        channel = message.channel
+        user = message.author
+
+        if not has_permission(user, 'manage_server'):
+            await client.send_message("no")
+            return
+
+        try:
+            days = int(args[0])
+        except:
+            days = 1
+
+        end = datetime.datetime.now() + datetime.timedelta(days=days)
+
+        storage = await self.get_storage(server)
+        if len(args) == 2 and args[1]:
+            await storage.set('double_xp:{}'.format(channel.id), days * 24 * 3600)
+        else:
+            await storage.set('double_xp:global'.format(channel.id), days * 24 * 3600)
+
+        try:
+            await self.client.delete_message(message)
+        except:
+            pass
+        await self.client.send_message(channel, "DOUBLE XP UNTIL {} !!".format(end.strftime(DATETIME_FORMAT)))
+
+    @command(pattern="^!doublexp$",
+             description="disable bonus xp",
+             usage="!doublexp")
+    async def disable_doublexp(self, message, args):
+        server = message.server
+        channel = message.channel
+        user = message.author
+
+        storage = await self.get_storage(server)
+        if any([
+                await storage.delete('double_xp:global'),
+                await storage.delete('double_xp:{}'.format(channel.id))]):
+            await self.client.send_message(channel, "DOUBLE XP ENDED.")
+
+    @command(pattern="^!level shutup$",
+             description="stop level announcements",
+             usage="!level shutup")
+    async def shutup(self, message: discord.Message, args: tuple=()):
+        server = message.server
+        channel = message.channel
+        user = message.author
+
+        if not has_permission(user, 'manage_server'):
+            await client.send_message("no")
+            return
+
+        storage = await self.get_storage(server)
+        await storage.set('announce_enabled', 0)
+
+        await self.client.send_message("Level up announcements disabled for {}".format())
 
     @command(pattern="^!xp ?(.*)?$",
              description="get xp for user",
@@ -95,7 +161,7 @@ class Levels(Plugin):
         storage = await self.get_storage(server)
         xp = await storage.get('player:{}:xp'.format(target.id)) or 0
         level = self.level_from_xp(int(xp))
-        next_level = self.xp_from_level(level)
+        next_level = self.xp_from_level(level + 1)
 
         await self.client.send_message(channel, "{user} is currently **level {level}** ({xp:,}/{next:,}) xp".format(
             user=target,
@@ -104,7 +170,22 @@ class Levels(Plugin):
             next=next_level
         ))
 
-    @command(pattern="^!levels$",
+    @command(pattern="^!skillcape",
+             description="get a skillcape when you're level 99")
+    async def get_skillcape(self, message: discord.Message, *_):
+        server = message.server  # type: discord.Server
+        channel = message.channel
+        user = message.author
+        storage = await self.get_storage(server)
+
+        xp = int(await storage.get('player:{}:xp'.format(user.id)) or 0)
+        lvl = self.level_from_xp(xp)
+        if lvl >= 120:
+            await self.client.send_message(channel, 'https://i.themork.co.uk/88220533c456.png')
+        elif lvl >= 99:
+            await self.client.send_message(channel, 'https://i.themork.co.uk/6cfc0a403220.png')
+
+    @command(pattern="^!levels(?: all)?$",
              description="view leaderboards for this server",
              usage="!levels",
              global_cooldown=5)
